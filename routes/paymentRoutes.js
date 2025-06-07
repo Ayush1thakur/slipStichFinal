da423c0f078c0cb106098b99ad27d9860b45404f
Ayush Thakur <134188820+Ayush1thakur@users.noreply.github.com>
2025-06-07 22:42:25 +0530

    Delete routes directory

--- routes/paymentRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const { makePayment } = require("../controllers/paymentController");
-const authenticate = require("../middleware/authM");
-
-router.post("/create-checkout-session", makePayment);
-
-module.exports = router;
