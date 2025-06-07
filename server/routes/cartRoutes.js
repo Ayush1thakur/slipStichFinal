d757fb64183f82f11e2d228643da89e9f639b820
Ayush Thakur <134188820+Ayush1thakur@users.noreply.github.com>
2025-06-07 22:50:15 +0530

    Add files via upload

--- server/routes/cartRoutes.js
+++ server/routes/cartRoutes.js
@@ -6,11 +6,13 @@ const {
   getCartItems,
   removeFromCart,
   updateCartQuantity,
+  clearCart,
 } = require("../controllers/cartController");
 
 router.post("/add", isUser, addToCart);
 router.get("/getCart", isUser, getCartItems);
 router.delete("/remove/:productId", isUser, removeFromCart);
 router.put("/update", isUser, updateCartQuantity);
+router.delete("/clear", isUser, clearCart);
 
 module.exports = router;
